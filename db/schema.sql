852b591ca5d923d4e1bbc69ad22968d9c875d7a9
JohnDJake <john.d.jake@gmail.com>
2021-01-22 19:20:48 -0800

    devoured default should've been false

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT TRUE,
+    devoured BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
