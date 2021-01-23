b150917c66bc513a9c0f953c0b8ed85869fc45bb
JohnDJake <john.d.jake@gmail.com>
2021-01-22 20:29:48 -0800

    created burger model using my orm

---
+++ models/burger.js
@@ -0,0 +1,13 @@
+// Import my orm
+const orm = require("../config/orm");
+
+const burger = {
+    // Get everything from the burgers table
+    selectAll(callback) { orm.selectAll("burgers", callback); },
+    // Insert one burger into the table
+    insertOne(objColsVals, callback) { orm.insertOne("burgers", objColsVals, callback); },
+    // Update one burger
+    updateOne(objColsVals, condition, callback) { orm.updateOne("burgers", objColsVals, condition, callback); }
+}
+
+module.exports = burger;
