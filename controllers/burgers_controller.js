2dee93e37b26b3a1a3d88796268319051a08111d
JohnDJake <john.d.jake@gmail.com>
2021-01-22 20:54:15 -0800

    created burgers controller with express routes

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,15 @@
+// Import express and create a router
+const router = require("express").Router();
+
+// Import burgers model
+const burgers = require("../models/burgers");
+
+// Create routes
+router.get("/", (req, res) => burgers.selectAll(burgers => res.render("index", { burgers })));
+
+router.post("/api/burgers", (req, res) => burgers.insertOne(req.body, ({ insertID: id }) => res.json(id)));
+
+router.put("/api/burgers/:id", (req, res) => burgers.updateOne(req.body, req.params, ({ changedRows }) => {
+    if (changedRows == 0) res.sendStatus(404);
+    else res.sendStatus(200);
+}));
