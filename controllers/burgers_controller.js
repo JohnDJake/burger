d3e52d0bfb3d61238bf688e3902159324fc8572b
JohnDJake <john.d.jake@gmail.com>
2021-01-24 18:46:00 -0800

    insertId, not insertID

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ const burgers = require("../models/burgers");
 // Create routes
 router.get("/", (req, res) => burgers.selectAll(burgers => res.render("index", { burgers })));
 
-router.post("/api/burgers", (req, res) => burgers.insertOne(req.body, ({ insertID: id }) => res.json(id)));
+router.post("/api/burgers", (req, res) => burgers.insertOne(req.body, ({ insertId: id }) => res.json(id)));
 
 router.put("/api/burgers/:id", (req, res) => burgers.updateOne(req.body, req.params, ({ changedRows }) => {
     if (changedRows == 0) res.sendStatus(404);
